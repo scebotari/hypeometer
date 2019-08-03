03ea2c9f6e7c837b9270f532a54a73b7c9e20cc2
Cebotari Sergei <cebotari.serghey@gmail.com>
2019-08-03 19:02:08 +0300

    Add production configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.7
 
-RUN pip install python-telegram-bot
+RUN pip install python-telegram-bot python-dotenv
 
 RUN mkdir /app
 ADD . /app
--- hype_bot.py
+++ hype_bot.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 from countdown import Countdown
 from datetime import datetime
+
+load_dotenv()
+mode = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
 
 # List of available commands
 # hype_level - Show the level of hype
@@ -33,15 +40,28 @@ def days_left_ru(bot, update):
 def next_event():
   return datetime(2019, 8, 13, 7, 0)
 
+if mode == "prod":
+  def run(updater):
+    PORT = int(os.environ.get("PORT", "8443"))
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+    # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+else:
+  def run(updater):
+    updater.start_polling()
+    updater.idle()
+
 def main():
-  updater = Updater('915978703:AAEq14kYuPXOGEdk-yHNAe7dllrTQwUEHCo')
+  updater = Updater(TOKEN)
+
   dp = updater.dispatcher
   dp.add_handler(CommandHandler('hype_level', hype_level_en))
   dp.add_handler(CommandHandler('days_left', days_left_en))
   dp.add_handler(CommandHandler('hype_level_ru', hype_level_ru))
   dp.add_handler(CommandHandler('days_left_ru', days_left_ru))
-  updater.start_polling()
-  updater.idle()
+
+  run(updater)
 
 if __name__ == '__main__':
   main()
